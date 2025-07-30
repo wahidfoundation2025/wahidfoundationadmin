5ebbe7448be9a11f6ed02ac7cf07d5d7169a1a99
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-07-30 10:43:51 +0530

    design

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -18,12 +18,12 @@ export default function LoginPage() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md bg-black/40">
       <div className="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-sm text-center animate-fade-in">
-        <h2 className="text-3xl font-bold mb-2 text-gray-800">Welcome to WahidAdmin</h2>
+        <h2 className="text-3xl font-bold mb-8 text-gray-800">Welcome to WahidAdmin</h2>
         <p className="text-sm text-gray-500 mb-6">Please sign in to continue</p>
 
         <button
           onClick={() => signIn("google")}
-          className="flex items-center justify-center gap-3 bg-white border border-gray-300 rounded-lg px-4 py-2 text-sm font-medium text-gray-700 hover:shadow transition"
+          className="flex w-full cursor-pointer hover:bg-gray-100 items-center justify-center gap-3 bg-white border border-gray-300 rounded-lg px-4 py-2 text-sm font-medium text-gray-700 hover:shadow transition"
         >
           <FcGoogle className="text-xl" />
           Sign in with Google
